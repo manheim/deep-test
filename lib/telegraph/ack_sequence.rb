94408a0ee03cd60c02f33a16c9d876bf34f6afa0
Ryan Rosenblum <ryan.rosenblum@gmail.com>
2016-10-05 15:45:35 -0400

    Fix deprecation warning. Use Mutex instead of Thread.exclusive

--- lib/telegraph/ack_sequence.rb
+++ lib/telegraph/ack_sequence.rb
@@ -1,11 +1,13 @@
 module Telegraph
   class AckSequence
+    MUTEX_FOR_ACK = Mutex.new
+
     def initialize
       @value = 0
     end
 
     def next
-      Thread.exclusive do
+      MUTEX_FOR_ACK.synchronize do
         @value += 1
       end
     end
